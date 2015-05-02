109d8653bcbf9edf22329544c5a8b680e9223112
Philippe.C <anoiaque@me.com>
2015-05-02 21:14:13 +0200

    boot file

---
+++ boot.rb
@@ -0,0 +1,10 @@
+lib = File.expand_path('../../lib', __FILE__)
+
+$LOAD_PATH.unshift(lib)
+ENV['BUNDLE_GEMFILE'] = File.expand_path('../Gemfile', __FILE__)
+
+require 'bundler/setup'
+require 'bundler'
+Bundler.require
+
+require 'scarabee'
