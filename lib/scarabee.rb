aaf12e4273e36761db7e6e8c20843497ab59fd6f
Philippe.C <anoiaque@me.com>
2015-05-03 17:05:22 +0200

    Handle pagination Handle exceptions at each command level

--- lib/scarabee.rb
+++ lib/scarabee.rb
@@ -1,53 +1,98 @@
 module Scarabee
+  class Exception < ::Exception
+    attr_reader :method, :url, :kind
+    def initialize method, url, kind
+      @method = method
+      @url = url
+      @kind = kind 
+    end
+  end
+  
   module Formatter
     def [] symbol
       symbol ? send(symbol) : none
     end
     
     def none
-      @@none ||= ->(text) { text }
+      @@none ||= ->(node) { node }
     end
     
     def price
-      @@price ||= ->(text) { text.match(/\d+[,\.\s]*\d*/)[0].gsub(/\s+/, '').gsub(/,/, '.').to_f  }
+      @@price ||= ->(node) { node.text.match(/\d+[,\.\s]*\d*/)[0].gsub(/\s+/, '').gsub(/,/, '.').to_f  }
     end
     
     def text
-      @@text ||= ->(text) { text.gsub(/\n|\t/, '').strip.gsub(/\s+/, ' ')  }
+      @@text ||= ->(node) { node.text.gsub(/\n|\t/, '').strip.gsub(/\s+/, ' ')  }
+    end
+    
+    def href
+      @@href ||= ->(node) { node.attributes['href'].value }
     end
     extend self
   end
   
   class Scraper
-    attr_reader :agent, :page, :entries
-  
+    attr_reader :entries, :page_count, :errors
+    
     def initialize agent: nil, &commands
       @commands = commands
       @agent = agent
       @entries = []
+      @page_count = 1
+      @errors = []
     end
     
-    def get xpath, as: :ivar
-      values = @page.search(xpath).map { |node| block_given? ? yield(node) : node }
+    def get xpath, as: :ivar, format: nil, array: true
+      nodes = @page.search(xpath)
+      values = nodes.map { |node| block_given? ? yield(node) : Formatter[format].(node) }
       
+      values = values.first unless array
       instance_variable_set "@#{as}", values
+      
+    rescue => e
+      @errors << Scarabee::Exception.new(:get, @page.uri.to_s, e)
     end
     
     def pick xpath, as: :key, format: nil
-      @page.search(xpath).each_with_index do |node, index|
+      nodes = @page.search(xpath)
+      
+      nodes.each_with_index { |node, index|
         @entries[index] ||= Hash.new
-        @entries[index][as] = Formatter[format].(node.text)
-      end
+        @entries[index][as] = Formatter[format].(node)
+      }
+      
+    rescue => e
+      @errors << Scarabee::Exception.new(:pick, @page.uri.to_s, e)
     end
     
     def open url
-      @page = agent.get url
+      @page = @agent.get url
+      
+    rescue => e
+      @errors << Scarabee::Exception.new(:open, @page.uri.to_s, e)
     end
     
-    def crawl urls, with: :scraper, only: (0..-1) 
+    def crawl urls, with: :scraper, only: (0..-1)
       urls = instance_variable_get "@#{urls}"
       
       urls[only].each { |url| with.run(url: url).each { |e| @entries << e.dup } }
+    
+    rescue => e
+      @errors << Scarabee::Exception.new(:crawl, @page.uri.to_s, e)
+    end
+    
+    def next_page ivar, stop: 0
+      return if stop == @page_count
+      return unless url = instance_variable_get("@#{ivar}")
+
+      @page_count += 1
+
+      if block_given? #yield entries and clear it to reduce memory usage
+        yield @entries
+        @entries = []
+      end
+      
+      instance_exec url, &@commands
     end
     
     def run url: nil
--- test/command_test.rb
+++ test/command_test.rb
@@ -59,4 +59,54 @@ class Scarabee::CommandTest < Minitest::Test
     assert entries[0][:city]
     assert entries[0][:price]
   end
+  
+  def test_crawl_over_pagination_and_scrape_each_page
+    yo = Scarabee.scraper do |url|
+      open url
+      pick 'span.price', as: :price, format: :price
+      pick 'td[itemprop="addressLocality"]', as: :city, format: :text
+    end
+    
+    lbc = Scarabee.scraper do |url|
+      open url
+      get 'div.list-lbc > a', as: :urls, format: :href
+      get '//li[@class="page"][last()]/a[1]', as: :next_page, format: :href, array: false
+      crawl :urls, with: yo, only: (0..1)
+      next_page :next_page, stop: 3
+    end
+    entries = lbc.run(url: URL)
+
+    assert_equal 6, entries.uniq.count
+    assert_equal 3, lbc.page_count
+  end
+  
+  def test_stop_unless_next_page_url
+    lbc = Scarabee.scraper do |url|
+      open url
+      get '//li[@class="page"][last()]/a[1]', as: :next_page, format: :href, array: false
+      next_page :next_page
+    end
+    
+    url = "http://www.leboncoin.fr/ventes_immobilieres/offres/ile_de_france/occasions/?o=34159000"
+    lbc.run(url: url)
+    
+    assert_equal 1, lbc.page_count
+  end
+  
+  def test_return_empty_array_on_failure_in_commands_run
+    Scarabee::Formatter.stubs(:[]).raises
+    
+    lbc = Scarabee.scraper do |url|
+      open url
+      get '//li[@class="page"][last()]/a[1]', as: :next_page, format: :href, array: false
+      next_page :next_page, stop: 3
+    end
+    
+    entries = lbc.run(url: URL)
+    e = lbc.errors.first
+    
+    assert_equal :get, e.method
+    assert_equal URL, e.url
+    assert e.kind.is_a?(RuntimeError)
+  end
 end
